278053b5a5e7fec4a33d70b7fdff398fc0c02d9b
Ken Dreyer <kdreyer@redhat.com>
2016-08-31 10:51:57 -0600

    setup.py: add "bump" command

--- setup.py
+++ setup.py
@@ -4,13 +4,68 @@ import subprocess
 import sys
 from setuptools.command.test import test as TestCommand
 from setuptools import Command
+try:
+    # Python 2 backwards compat
+    from __builtin__ import raw_input as input
+except ImportError:
+    pass
 
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 LONG_DESCRIPTION = open(readme).read()
 
-module_file = open('errata_tool/__init__.py').read()
+
+def read_module_contents():
+    with open('errata_tool/__init__.py') as app_init:
+        return app_init.read()
+
+
+def read_spec_contents():
+    with open('python-errata-tool.spec') as spec:
+        return spec.read()
+
+module_file = read_module_contents()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
 version = metadata['version']
+
+
+class BumpCommand(Command):
+    """ Bump the __version__ number and commit all changes. """
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        version = metadata['version'].split('.')
+        version[-1] = str(int(version[-1]) + 1)  # Bump the final part
+
+        print('old version: %s  new version: %s' %
+              (metadata['version'], '.'.join(version)))
+        try:
+            input('Press enter to confirm, or ctrl-c to exit >')
+        except KeyboardInterrupt:
+            raise SystemExit("\nNot proceeding")
+
+        old = "__version__ = '%s'" % metadata['version']
+        new = "__version__ = '%s'" % '.'.join(version)
+        module_file = read_module_contents()
+        with open('errata_tool/__init__.py', 'w') as fileh:
+            fileh.write(module_file.replace(old, new))
+
+        old = 'Version:        %s' % metadata['version']
+        new = 'Version:        %s' % '.'.join(version)
+        spec_file = read_spec_contents()
+        with open('python-errata-tool.spec', 'w') as fileh:
+            fileh.write(spec_file.replace(old, new))
+
+        # Commit everything with a standard commit message
+        cmd = ['git', 'commit', '-a', '-m', 'version %s' % '.'.join(version)]
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
 
 
 class ReleaseCommand(Command):
@@ -89,5 +144,5 @@ setup(
         'pytest-flake8',
         'httpretty',
     ],
-    cmdclass={'test': PyTest, 'release': ReleaseCommand},
+    cmdclass={'test': PyTest, 'bump': BumpCommand, 'release': ReleaseCommand},
 )
