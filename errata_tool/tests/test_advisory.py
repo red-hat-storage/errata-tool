a4708bb6c326ec8f988797914f12bfba9e0e81eb
Ken Dreyer <kdreyer@redhat.com>
2017-03-23 10:38:41 -0600

    tests: add functional Erratum test
    Add unit tests for the basic GET API.
    
    Import fixture JSON data from an example ET advisory.
    
    Add a shell script that can quickly grab other ET URLs and convert them
    into fixtures.

---
+++ errata_tool/tests/test_advisory.py
@@ -0,0 +1,123 @@
+import json
+import os
+from errata_tool import ErrataConnector, Erratum
+import requests
+import pytest
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
+
+
+class MockResponse(object):
+    status_code = 200
+    encoding = 'utf-8'
+    headers = {'content-type': 'application/json; charset=utf-8'}
+
+    def raise_for_status(self):
+        pass
+
+    @property
+    def _fixture(self):
+        """ Return path to our static fixture file. """
+        return self.url.replace('https://errata.devel.redhat.com/',
+                                os.path.join(FIXTURES_DIR,
+                                             'errata.devel.redhat.com/'))
+
+    def json(self):
+        try:
+            with open(self._fixture) as fp:
+                return json.load(fp)
+        except IOError:
+            print('Try ./new-fixture.sh %s' % self.url)
+            raise
+
+    @property
+    def text(self):
+        """ Return contents of our static fixture file. """
+        try:
+            with open(self._fixture) as fp:
+                return fp.read()
+        except IOError:
+            print('Try ./new-fixture.sh %s' % self.url)
+            raise
+
+
+def mock_get(url, **kwargs):
+    """ mocking requests.get() """
+    m = MockResponse()
+    m.url = url
+    return m
+
+
+@pytest.fixture
+def advisory(monkeypatch):
+    monkeypatch.delattr('requests.sessions.Session.request')
+    monkeypatch.setattr(ErrataConnector, '_auth', None)
+    monkeypatch.setattr(requests, 'get', mock_get)
+    return Erratum(errata_id=26175)
+
+
+class TestAdvisory(object):
+
+    def test_instance(self, advisory):
+        assert isinstance(advisory, Erratum)
+
+    def test_errata_state(self, advisory):
+        assert advisory.errata_state == 'SHIPPED_LIVE'
+
+    def test_url(self, advisory):
+        expected = 'https://errata.devel.redhat.com/advisory/26175'
+        assert advisory.url() == expected
+
+    def test_synopsis(self, advisory):
+        expected = 'Red Hat Ceph Storage 2.2 bug fix and enhancement update'
+        assert advisory.synopsis == expected
+
+    def test_topic(self, advisory):
+        expected = 'Red Hat Ceph Storage 2.2 is now available.'
+        assert advisory.topic == expected
+
+    def test_description(self, advisory):
+        expected = 'Red Hat Ceph Storage is a scalable'
+        assert advisory.description.startswith(expected)
+
+    def test_solution(self, advisory):
+        expected = 'Before applying'
+        assert advisory.solution.startswith(expected)
+
+    def test_qe_email(self, advisory):
+        assert advisory.qe_email == 'hnallurv@redhat.com'
+
+    def test_qe_group(self, advisory):
+        assert advisory.qe_group == 'Default'
+
+    def test_errata_type(self, advisory):
+        assert advisory.errata_type == 'RHBA'
+
+    def test_owner_email(self, advisory):
+        assert advisory.package_owner_email == 'kdreyer@redhat.com'
+
+    def test_manager_email_is_none(self, advisory):
+        # NOTE: the ET does not give this information when querying a
+        # pre-existing advisory. We can only update it with POST/PUT, but we
+        # cannot GET it.
+        #  assert advisory.manager_email == 'gmeno@redhat.com'
+        assert advisory.manager_email is None
+
+    def test_text_only(self, advisory):
+        assert advisory.text_only is False
+
+    def test_publish_date_override(self, advisory):
+        assert advisory.publish_date_override is None
+
+    def test_errata_bugs(self, advisory):
+        # Only sanity-check one for brevity.
+        assert 1425771 in advisory.errata_bugs
+        assert len(advisory.errata_bugs) == 61
+
+    def test_errata_builds(self, advisory):
+        expected = {'RHEL-7-CEPH-2': ['ceph-10.2.5-37.el7cp']}
+        assert advisory.errata_builds == expected
+
+    def test_current_flags(self, advisory):
+        assert advisory.current_flags == []
---
+++ new-fixture.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Easy way to quickly add more tests using real API data.
+# Please only use advisories that have already shipped live.
+
+set -ex
+
+if [ -z $1 ]; then
+  echo "Specify an ET API URL."
+  echo "Examples:"
+  echo "  https://errata.devel.redhat.com/advisory/26175.json"
+  echo "  https://errata.devel.redhat.com/api/v1/erratum/26175"
+  echo "Please only use advisories that have already shipped live."
+  exit 1
+fi
+
+URL=$1
+
+FIXTURE=$(echo $URL | sed 's|^https://||')
+DIRNAME=$(dirname $FIXTURE)
+
+cd errata_tool/tests/fixtures/
+mkdir -p $DIRNAME
+curl --negotiate -u : -q $URL > $FIXTURE
+
+git add $FIXTURE
