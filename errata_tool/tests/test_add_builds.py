0cc0b16a6f29d31c3c2b3e2ad4eb313b010f7806
Ken Dreyer <kdreyer@redhat.com>
2017-09-29 11:15:02 -0600

    test addBuilds() method

---
+++ errata_tool/tests/test_add_builds.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class TestAddBuilds(object):
+
+    def test_add_builds_url(self, monkeypatch, mock_post, advisory):
+        monkeypatch.setattr(requests, 'post', mock_post)
+        advisory.addBuilds(['ceph-10.2.3-17.el7cp'], release='RHEL-7-CEPH-2')
+        assert mock_post.response.url == 'https://errata.devel.redhat.com/api/v1/erratum/26175/add_builds'  # NOQA: E501
+
+    def test_builds_data(self, monkeypatch, mock_post, advisory):
+        monkeypatch.setattr(requests, 'post', mock_post)
+        advisory.addBuilds(['ceph-10.2.3-17.el7cp'], release='RHEL-7-CEPH-2')
+        expected = {
+            "product_version": "RHEL-7-CEPH-2",
+            "build": "ceph-10.2.3-17.el7cp",
+        }
+        assert mock_post.kwargs['json'] == [expected]
