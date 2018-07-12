e8946fb8a6edeb958860811c2bf1784736002c30
Ken Dreyer <kdreyer@redhat.com>
2018-07-12 11:47:54 -0600

    setup.py: upload during "release" operation
    Generate the source tarball and upload it to PyPI during
    "python setup.py release"

--- setup.py
+++ setup.py
@@ -94,15 +94,28 @@ class ReleaseCommand(Command):
         print(' '.join(cmd))
         subprocess.check_call(cmd)
 
-        # Push package to pypi
-        # cmd = ['python', 'setup.py', 'sdist', 'upload']
-        # if self.sign:
-        #    cmd.append('--sign')
-        # print(' '.join(cmd))
-        # subprocess.check_call(cmd)
-
         # Push master to the remote
         cmd = ['git', 'push', 'origin', 'master']
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
+
+        # Create source package
+        cmd = ['python', 'setup.py', 'sdist']
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
+
+        tarball = 'dist/%s-%s.tar.gz' % ('errata-tool', version)
+
+        # GPG sign
+        if self.sign:
+            cmd = ['gpg2', '-b', '-a', tarball]
+            print(' '.join(cmd))
+            subprocess.check_call(cmd)
+
+        # Upload
+        cmd = ['twine', 'upload', tarball]
+        if self.sign:
+            cmd.append(tarball + '.asc')
         print(' '.join(cmd))
         subprocess.check_call(cmd)
 
