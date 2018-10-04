0f774cd547c1bfba3683a7422b6edd0ca4af5659
Ken Dreyer <kdreyer@redhat.com>
2018-10-04 10:34:47 -0600

    tests: update ProductVersion test fixture
    Use a more up-to-date ProductVersion test fixture "RHEL-7-RHCEPH-3.1",
    instead of the older, now defunct "RHEL-7-CEPH-3".
    
    This allows us to add more functionality and test fixtures using an
    up-to-date example of a Product Version.

--- errata_tool/tests/test_product_version.py
+++ errata_tool/tests/test_product_version.py
@@ -1,11 +1,11 @@
 def test_id(product_version):
-    assert product_version.id == 665
+    assert product_version.id == 783
 
 
 def test_name(product_version):
-    assert product_version.name == 'RHEL-7-CEPH-3'
+    assert product_version.name == 'RHEL-7-RHCEPH-3.1'
 
 
 def test_description(product_version):
-    expected = 'Red Hat Ceph Storage 3 for Red Hat Enterprise Linux 7'
+    expected = 'Red Hat Ceph Storage 3.1'
     assert product_version.description == expected
