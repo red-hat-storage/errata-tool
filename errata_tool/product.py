0006ee91f5b3e11fc2996eda5c390530972d2815
Joshua Stone <jostone@redhat.com>
2021-06-17 12:13:43 -0400

    Add product_versions() to Product class
    This change introduces a product_versions() method to the Product
    class for returning a list of product versions associated with a
    product.

--- errata_tool/product.py
+++ errata_tool/product.py
@@ -1,11 +1,29 @@
 from errata_tool import ErrataConnector
-
+from errata_tool.product_version import ProductVersion
 
 class Product(ErrataConnector):
     def __init__(self, name):
         self.name = name
         self.data = None
         self.url = self._url + '/products/%s' % self.name
+
+    def product_versions(self):
+        """Get the list of product version for this Product.
+
+        :returns: a (possibly-empty) list of ProductVersion objects.
+        """
+        url = '/api/v1/products/%s/product_versions' % self.name
+        result = self._get(url)['data']
+        product_versions = []
+        for product_version in result:
+            product_version_id = product_version['id']
+            product_versions.append(
+                ProductVersion(
+                    id_or_name=product_version_id,
+                    data=product_version
+                )
+            )
+        return product_versions
 
     def refresh(self):
         url = '/api/v1/products/' + self.name
